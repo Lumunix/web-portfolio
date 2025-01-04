7b07abb38882351bf86ca98338e8a26400a301bf
Lumunix <lumunix@icloud.com>
2025-01-04 14:20:41 -0500

    new

---
+++ src/components/projects/ProjectsFilter.jsx
@@ -0,0 +1,44 @@
+const selectOptions = [
+	'Web Application',
+	'Mobile Application',
+	'UI/UX Design',
+	'Branding',
+];
+
+const ProjectsFilter = ({ setSelectProject }) => {
+	return (
+		<select
+			onChange={(e) => {
+				setSelectProject(e.target.value);
+			}}
+			className="font-general-medium 
+                px-4
+                sm:px-6
+                py-2
+                border
+                dark:border-secondary-dark
+                rounded-lg
+                text-sm
+                sm:text-md
+                dark:font-medium
+                bg-secondary-light
+                dark:bg-ternary-dark
+                text-primary-dark
+                dark:text-ternary-light
+            "
+		>
+			<option value="" className="text-sm sm:text-md">
+                All Projects
+            </option>
+
+            {/* Add value prop to dynamically generated options */}
+            {selectOptions.map((option) => (
+                <option value={option} className="text-normal sm:text-md" key={option}>
+                    {option}
+                </option>
+            ))}
+		</select>
+	);
+};
+
+export default ProjectsFilter;
