92ae7cf398e24f90b78fa65e4e58bd8e81e8316f
Lumunix <lumunix@icloud.com>
2025-01-05 18:41:35 -0500

    add

--- src/components/contact/ContactDetails.jsx
+++ src/components/contact/ContactDetails.jsx
@@ -3,17 +3,17 @@ import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
+		name: 'Nashua, NH',
 		icon: <FiMapPin />,
 	},
 	{
 		id: 2,
-		name: 'email@domain.com',
+		name: 'dylanpoljak@icloud.com',
 		icon: <FiMail />,
 	},
 	{
 		id: 3,
-		name: '555 8888 888',
+		name: '724-678-5948',
 		icon: <FiPhone />,
 	},
 ];
